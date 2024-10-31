e10802ac50c01fdfc859f9b5c5262deb95cc6d9d
emanuelarevalomartinez <emanuelarevalomartinez@gmail.com>
2024-10-30 22:51:19 -0400

    primer comit del proyecto cosas basicas para ver como se ve

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+
+
+function App() {
+
+  return (
+    <>
+     <div className="flex h-screen w-screen bg-[#303030] text-white items-center justify-center">
+      <p className="flex text-2xl text-center">No te asustes esta es la Primera vista de la  pagina web que va a ir creciendo </p>
+     </div>
+    </>
+  )
+}
+
+export default App
