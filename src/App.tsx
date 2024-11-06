3c475a2770dc7aaf15632e37aba2635dd7947058
emanuelarevalomartinez <emanuelarevalomartinez@gmail.com>
2024-11-06 13:57:52 -0500

    se añadiieron algunas de las imagenes a usar, se añadio el navbar para la  barra  de navegacion

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
-
+import image from '../public/cuba/banderas/wp8347532-flag-cuba-wallpapers.jpg'
 
 function App() {
 
   return (
     <>
-     <div className="flex h-screen w-screen bg-[#303030] text-white items-center justify-center">
-      <p className="flex text-2xl text-center">No te asustes esta es la Primera vista de la  pagina web que va a ir creciendo  </p>
+     <div className="flex min-h-screen w-screen bg-[#3572EF] text-[#A7E6FF]">
+     <p>hola</p>
+     <div>
+      <img 
+      className='h-40'
+      src={image} alt="It is no t possible to view the image" />
+     </div>
      </div>
     </>
   )
